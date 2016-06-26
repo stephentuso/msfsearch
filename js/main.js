c2527913690e0e7d6266357a2b3c9ca68b8fcb54
ryanyue123 <yue.ryan123@gmail.com>
2016-06-26 08:08:05 -0700

    Allows readding of the same file

--- js/main.js
+++ js/main.js
@@ -32,7 +32,6 @@ function queryDataPhonetic(data, query_string) {
 function convertToPhonetic(string) {
     return doubleMetaphone(string)[0];
 }
-
 function parseCsvFile(file, callback) {
     var parser = new SimpleExcel.Parser.CSV();
     parser.loadFile(file, function() {
@@ -67,22 +66,3 @@ function parseCsvFile(file, callback) {
         callback(data);
     });
 }
-
-// Fuzzy search
-
-// function queryData(data, query_string) {
-//     var output_data = []
-//     var options = {
-//         caseSensitive: false,
-//         includeScore: true,
-//         shouldSort: true,
-//         tokenize: false,
-//         keys: ["original", "phonetic", "column_name"],
-//     }
-//     for (var i=0; i<data.length; i++) {
-//         var fuse = new Fuse(data[i], options)
-//         var result = fuse.search(query_string)
-//         output_data = output_data.concat(result)
-//     }
-//     console.log(output_data)
-// }
