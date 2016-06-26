3e42008d95c7d10de05c8823473824d03a3a98d9
Jonathan Lam <lamj18@uci.edu>
2016-06-25 20:39:25 -0700

    UI

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
+function processFiles(files) {
+    for (var i = 0; i < files.length; i++) {
+        (function(index) {
+            parseCsvFile(files[index], function(data) {
+                allData = allData.concat(data);
+                if (index == files.length - 1) {
+                    doneProcessing();
+                }
+            });
+        })(i);
+    }
+}
+
+function doneProcessing() {
+    console.log(allData);
+    window.localStorage.setItem("allData", allData);
+    var data = queryDataPhonetic(allData, "jiga");
+    console.log(data);
+}
+
 function queryDataPhonetic(data, query_string) {
-    query_string = convertToPhonetic(query_string)
-    var query_results = []
+    query_string = convertToPhonetic(query_string);
+    console.log(query_string);
+    var query_results = [];
     for (var i=0; i<data.length; i++) {
         for (var j=0; j<data[i].length; j++) {
             if (data[i][j]["phonetic"] == query_string) {
-                query_results.push(data[i])
+                query_results.push(data[i]);
+                console.log(data[i][28]["original"]);
+                console.log(data[i][j].phonetic);
             }
         }
     }
@@ -26,8 +49,6 @@ function parseCsvFile(file, callback) {
             var cell = sheet[0][i];
             headers.push(cell.toString());
         }
-        console.log(headers);
-        console.log(sheet[0]);
 
         for (var i = 0; i < sheet.length; i++) {
            var row = sheet[i];
@@ -46,6 +67,10 @@ function parseCsvFile(file, callback) {
 
         callback(data);
     });
+}
+
+function fuzzySearch(array) {
+
 }
 
 // Fuzzy search
